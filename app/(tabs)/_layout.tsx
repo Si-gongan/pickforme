a3b719022d6c33ed9386198562805ca3e2f7141c
jin60641 <jin60641@naver.com>
2023-05-15 10:49:49 +0900

    라우팅, 메인퍼블리싱

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,11 @@
+import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
+import { useRouter, Link, Tabs } from 'expo-router';
 import { Pressable, useColorScheme } from 'react-native';
+
+import { useAtomValue } from 'jotai';
+import { Text } from '../../components/Themed';
+import { userDataAtom } from '../../stores/auth/atoms';
 
 import Colors from '../../constants/Colors';
 
@@ -16,17 +21,38 @@ function TabBarIcon(props: {
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const router = useRouter();
+  const userData = useAtomValue(userDataAtom);
+
+  React.useEffect(() => {
+    if (!userData) {
+      router.replace('register');
+    }                 
+  }, [userData]);             
+  if (!userData) {     
+    return null;
+  }                   
 
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShadowVisible: false, // applied here
+        headerStyle: {
+        },
+        headerLeft: () => (
+          <Text>
+            픽포미
+          </Text>
+        ),
+        headerTintColor: 'transparent',
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: '홈',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          /*
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -41,12 +67,27 @@ export default function TabLayout() {
               </Pressable>
             </Link>
           ),
+          */
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="discover"
         options={{
-          title: 'Tab Two',
+          title: 'AI 포미',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: '의뢰 목록',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="mypage"
+        options={{
+          title: '마이페이지',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Pressable, FlatList } from 'react-native';
+import { useRouter } from 'expo-router';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
+  const router = useRouter();
+  const onPress = (id: string) => {
+    router.push(id);
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>곰지님 안녕하세요, 좋은 하루입니다!</Text>
+      <Text style={styles.title}>픽포미 쇼핑 도우미 서비스를 이용해보세요.</Text>
+      <View style={styles.save}>
+      <FlatList<string>
+        contentContainerStyle={styles.list}
+        data={['recommend', 'research', 'buy', 'AI']}
+        numColumns={2}
+        columnWrapperStyle={styles.row}
+        renderItem={({ item }) => (
+          <Pressable onPress={() => onPress(item)}>
+            <View style={styles.view}>
+              <Text style={styles.text}>{item}</Text>
+            </View>
+          </Pressable>
+        )}
+        keyExtractor={(item) => item}
+      />
+      </View>
     </View>
   );
 }
@@ -19,13 +38,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  save: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  list: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  view: {
+    backgroundColor: '#111E4F',
+    flexDirection: 'column',
+    margin: 12,
+    width: 122,
+    height: 172,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowColor: 'rgba(17, 30, 79)',
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    color: '#e9e9e9',
+    marginTop: 12,
+    fontWeight: '600',
+    fontSize: 18,
+    lineHeight: 22,
+  },
+  row: {
+    justifyContent: 'space-between',
+  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
