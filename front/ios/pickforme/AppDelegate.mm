4dfbbe61d2c2d59aedc94c7bdccc6bfd432ac60e
ever2after <dhwntkd412@snu.ac.kr>
2024-03-09 01:05:23 +0900

    feat: firebase sdk

--- front/ios/pickforme/AppDelegate.mm
+++ front/ios/pickforme/AppDelegate.mm
@@ -4,6 +4,9 @@
 #import <React/RCTLinkingManager.h>
 #import <RNKakaoLogins.h>
 
+#import <Firebase.h>
+
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -13,9 +16,13 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  if([FIRApp defaultApp] == nil)
+    [FIRApp configure];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
