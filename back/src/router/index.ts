25c1f74614123ae913a34eab967c78622c9d9eed
jin60641 <jin60641@naver.com>
2024-05-09 11:21:25 +0900

    export스크립트 추가

--- back/src/router/index.ts
+++ back/src/router/index.ts
@@ -20,4 +20,37 @@ const router = new Router();
   router.use(subrouter.routes());
 });
 
+import db from '../models';
+import * as fs from 'fs';
+router.get('/export', async (ctx) => {
+  const requests = await db.Request.find({
+  }).populate('userId').sort({
+    createdAt: -1,
+  });
+  const data = requests.map((request) => ({
+    id: request._id.toString(),
+    type: request.type,
+    email: request.userId.email,
+    name:request.name,
+    text:request.text,
+    review: request.rating ?? '없음',
+  }));
+  const array = data;
+
+  const convertToCSV = (arr: Array<{ [key: string]: any }>) => {
+    const header = Object.keys(arr[0]).join(',') + '\n';
+    const content = arr
+        .map((row) => Object.values(row).join(','))
+        .join('\n');
+    return header + content + '\n';
+  };
+  const saveToCSV = (data: string, fileName: string) => {
+    fs.writeFileSync(fileName, data);
+  };
+
+  const csvData = convertToCSV(array);
+  saveToCSV(csvData, 'output.csv');
+  ctx.body = csvData;
+});
+
 export default router;
