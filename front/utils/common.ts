5c3fff91ef1fadd046b359e7e77b33f8dec2d384
ever2after <dhwntkd412@snu.ac.kr>
2024-05-15 21:35:38 +0900

    fix: 예외처리

--- front/utils/common.ts
+++ front/utils/common.ts
@@ -1,7 +1,10 @@
 export const hexToRgb = (hex: string) =>
   /^#?([a-fA-F\d]{2})([a-fA-F\d]{2})([a-fA-F\d]{2})$/i.exec(hex)?.slice(1, 4).map(a => parseInt(a, 16)).join(',');
 
-export const numComma = (num: number) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+export const numComma = (num: number) => {
+  if (num === undefined) return '0';
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+} 
 
 export const formatDate = (date: string | Date) => {
   const today = new Date(date);
