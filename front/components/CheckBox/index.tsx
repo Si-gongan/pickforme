70e5e217387aef5e84a0d4e7e2a7d2d51bcb0ae0
권재원 <springkjw@gmail.com>
2025-02-12 15:09:50 +0900

    🏗️ [front]체크박스 리팩토링

---
+++ front/components/CheckBox/index.tsx
@@ -0,0 +1,27 @@
+/**
+ * 체크박스 컴포넌트
+ */
+import { Pressable, View, Image } from "react-native";
+
+import useStyles from "./style";
+
+import type { ICheckBoxProps } from "./type";
+
+export default function CheckBox({
+  checked,
+  onPress,
+  ...props
+}: ICheckBoxProps) {
+  const styles = useStyles();
+
+  return (
+    <Pressable onPress={onPress} accessibilityRole="checkbox" {...props}>
+      <View style={[styles.wrap, checked && styles.checked]}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/check.png")}
+        />
+      </View>
+    </Pressable>
+  );
+}
