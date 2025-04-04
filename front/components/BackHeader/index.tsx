63d04daf87107301923dd0c08bf77cfadf7860d5
seonggook-cho <seonggook.cho@gmail.com>
2025-04-04 12:15:29 +0900

    BackHeader 디자인 수정

--- front/components/BackHeader/index.tsx
+++ front/components/BackHeader/index.tsx
@@ -1,34 +1,35 @@
-import { useCallback } from "react";
-import { View, TouchableOpacity, Image } from "react-native";
-import { useRouter } from "expo-router";
+import { useCallback } from 'react';
+import { TouchableOpacity, Image } from 'react-native';
+import { useRouter } from 'expo-router';
+import { View } from '../Settings/Themed'; // Themed View 사용
 
-import { BackImage } from "@assets";
-import useStyle from "./style";
+import { BackImage } from '@assets';
+import useStyle from './style';
 
 export default function BackHeader() {
-  const router = useRouter();
-  const style = useStyle();
+    const router = useRouter();
+    const style = useStyle();
 
-  const onPress = useCallback(
-    function () {
-      if (router.canGoBack()) {
-        router.back();
-      }
-    },
-    [router]
-  );
+    const onPress = useCallback(
+        function () {
+            if (router.canGoBack()) {
+                router.back();
+            }
+        },
+        [router]
+    );
 
-  return (
-    <View style={style.BackHeader}>
-      <TouchableOpacity
-        style={style.BackHeaderButton}
-        onPress={onPress}
-        accessible
-        accessibilityRole="button"
-        accessibilityLabel="뒤로가기"
-      >
-        <Image style={style.BackHeaderImage} source={BackImage} />
-      </TouchableOpacity>
-    </View>
-  );
+    return (
+        <View style={style.BackHeader}>
+            <TouchableOpacity
+                style={style.BackHeaderButton}
+                onPress={onPress}
+                accessible
+                accessibilityRole="button"
+                accessibilityLabel="뒤로가기"
+            >
+                <Image style={style.BackHeaderImage} source={BackImage} />
+            </TouchableOpacity>
+        </View>
+    );
 }
