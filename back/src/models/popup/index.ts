9662115566d0990f8240613ddc6ab57417b009bd
newtonjjang <newtonjjang@naver.com>
2025-04-16 17:54:41 +0900

    [server] feat: popup 모델 추가

---
+++ back/src/models/popup/index.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const popupSchema = new mongoose.Schema({
+  popup_id: {
+    type: String,
+    required: true,
+    unique: true, // 사람이 읽을 수 있는 고유 ID로 설정해두면 됩니다.
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
+const Popup = mongoose.models.Popup || mongoose.model("Popup", popupSchema);
+
+export default Popup;
