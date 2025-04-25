41e0b0bb97f4be158d388cd5f20b7a89e0cb3e10
newtonjjang <newtonjjang@naver.com>
2025-04-25 16:40:47 +0900

    [server] fix: remove /purchase/check api

--- back/src/router/purchase/index.ts
+++ back/src/router/purchase/index.ts
@@ -93,48 +93,6 @@ router.get('/products/:platform', async (ctx) => {
   // ctx.body = [];
   ctx.status = 200;
 });
-// 구독 여부 체크
-router.get('/check', requireAuth, async (ctx) => {
-  const subscription = await db.Purchase.findOne({
-    userId: ctx.state.user._id,
-    isExpired: false,
-    'product.type': ProductType.SUBSCRIPTION,
-  }).sort({
-    createdAt: -1,
-  });
-
-  const user = await db.User.findById(ctx.state.user._id);
-  if (subscription === null || user === null) {
-    ctx.status = 400;
-    ctx.body = {
-      error: 'Subscription or user not found',
-    };
-    return;
-  }
-
-  const oneMonthLater = new Date(subscription.createdAt);
-  oneMonthLater.setMonth(oneMonthLater.getMonth() + 1);
-  const now = new Date();
-
-  if (oneMonthLater < now) {
-    await subscription.updateExpiration();
-    await user.processExpiredMembership();
-    ctx.status = 400;
-    ctx.body = {
-      error: 'Subscription expired',
-    };
-  } else {
-    const timeLeft = oneMonthLater.getTime() - now.getTime();
-    const daysLeft = timeLeft / (1000 * 60 * 60 * 24);
-
-    ctx.status = 200;
-    ctx.body = {
-      status: 'active',
-      daysLeft: `${daysLeft.toFixed(2)}일`,
-      expiresAt: oneMonthLater.toISOString(),
-    };
-  }
-});
 
 router.get('/purchases', requireAuth, async (ctx) => {
   const purchases = await db.Purchase.find({
