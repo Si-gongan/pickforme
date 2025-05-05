af97e045efd2e7171b8e533d5a6d158f913cee25
seonggook-cho <seonggook.cho@gmail.com>
2025-05-06 00:09:24 +0900

    auth api 에러 전파 중지되는 문제 수정

--- front/stores/auth/apis.ts
+++ front/stores/auth/apis.ts
@@ -13,31 +13,15 @@ import {
     Popup
 } from './types';
 
-export const AppleLoginAPI = (params: AppleLoginParams) =>
-    client.post<LoginResponse>('/auth/apple', params).catch(error => {
-        console.log(error);
-    });
+export const AppleLoginAPI = (params: AppleLoginParams) => client.post<LoginResponse>('/auth/apple', params);
 export const KakaoLoginAPI = (params: KakaoLoginParams) => client.post<LoginResponse>('/auth/kakao', params);
-export const GoogleLoginAPI = (params: GoogleLoginParams) =>
-    client.post<LoginResponse>('/auth/google', params).catch(error => {
-        console.log(error);
-    });
-export const SetPushTokenAPI = (params: SetPushTokenParams) =>
-    client.post('/auth/pushtoken', params).catch(error => {
-        console.log(error);
-    });
+export const GoogleLoginAPI = (params: GoogleLoginParams) => client.post<LoginResponse>('/auth/google', params);
+export const SetPushTokenAPI = (params: SetPushTokenParams) => client.post('/auth/pushtoken', params);
 export const SetPushSettingAPI = (params: SetPushSettingParams) =>
-    client.put<SetPushSettingResponse>('/auth/pushsetting', params).catch(error => {
-        console.log(error);
-    });
-export const QuitAPI = () =>
-    client.post('/auth/quit').catch(error => {
-        console.log(error);
-    });
+    client.put<SetPushSettingResponse>('/auth/pushsetting', params);
+export const QuitAPI = () => client.post('/auth/quit');
 export const PhoneCheckAPI = async ({ id, phone }: phoneCheckParams) => {
-    console.log('API 호출:', { phone });
     const response = await client.post('/user/duplicationphone', { phone });
-    console.log('API 응답:', response.data);
     return response;
 };
 
@@ -45,8 +29,6 @@ export const SetPopupAPI = (params: SetPopupParams) => client.post('/user/setpop
 export const GetPopupAPI = () => client.get<Popup[]>('/popup/active');
 
 export const PhoneSubmitAPI = async ({ id, phone }: phoneCheckParams) => {
-    console.log('API 호출:', { id, phone });
     const response = await client.post('/user/phone', { id, phone });
-    console.log('API 응답:', response.data);
     return response;
 };
