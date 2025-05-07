2048b308f1b0c3d61443b9b8ad2e83f365bbb4ef
newtonjjang <newtonjjang@naver.com>
2025-05-07 12:28:06 +0900

    [server] feat: jest 테스트 시간 단축관련 설정 변경 & 테스트 파일명 변경

--- back/jest.config.js
+++ back/jest.config.js
@@ -13,4 +13,36 @@ module.exports = {
   },
   testMatch: ['**/__tests__/**/*.test.ts'],
   setupFiles: ['<rootDir>/src/__tests__/setup.ts'],
+  verbose: true,
+  // transformIgnorePatterns: ['node_modules/'], // 모든 node_modules 변환 무시
+  projects: [
+    {
+      displayName: 'unit',
+      testMatch: ['**/__tests__/**/*.unit.test.ts'],
+      moduleNameMapper: {
+        '^models$': '<rootDir>/src/models',
+        '^models/(.*)$': '<rootDir>/src/models/$1',
+        '^utils$': '<rootDir>/src/utils',
+        '^utils/(.*)$': '<rootDir>/src/utils/$1',
+        '^scheduler$': '<rootDir>/src/scheduler',
+        '^scheduler/(.*)$': '<rootDir>/src/scheduler/$1',
+        '^socket$': '<rootDir>/src/socket.ts',
+        '^socket/(.*)$': '<rootDir>/src/socket.ts',
+      },
+    },
+    {
+      displayName: 'integration',
+      testMatch: ['**/__tests__/**/*.integration.test.ts'],
+      moduleNameMapper: {
+        '^models$': '<rootDir>/src/models',
+        '^models/(.*)$': '<rootDir>/src/models/$1',
+        '^utils$': '<rootDir>/src/utils',
+        '^utils/(.*)$': '<rootDir>/src/utils/$1',
+        '^scheduler$': '<rootDir>/src/scheduler',
+        '^scheduler/(.*)$': '<rootDir>/src/scheduler/$1',
+        '^socket$': '<rootDir>/src/socket.ts',
+        '^socket/(.*)$': '<rootDir>/src/socket.ts',
+      },
+    }
+  ]
 }; 
