60dbf8b74a1d567280caa932ad2307102583f33c
seonggook-cho <seonggook.cho@gmail.com>
2025-06-09 15:02:47 +0900

    한시련 페이지 체크박스 디자인 수정

--- front/components/CheckBox/style.ts
+++ front/components/CheckBox/style.ts
@@ -1,25 +1,12 @@
 import { StyleSheet } from 'react-native';
-import useColorScheme from '../../hooks/useColorScheme';
-import Colors from '../../constants/Colors';
 
 export default function useStyles() {
-    const colorScheme = useColorScheme();
-
     return StyleSheet.create({
         wrap: {
-            // borderWidth: 1,
-            // borderColor: Colors[colorScheme].buttonBackground.secondary,
-            width: 20,
-            height: 20,
+            width: 24, 
+            height: 24, 
             alignItems: 'center',
             justifyContent: 'center'
-        },
-        checked: {
-            backgroundColor: 'white'
-        },
-        image: {
-            width: 20,
-            height: 20
         }
     });
 }
